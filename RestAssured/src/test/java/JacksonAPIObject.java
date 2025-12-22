0c13de877e3b214921d00225e64acdbfe91afcf1
rohit <rohitpachauri2@gmail.com>
2025-12-22 08:26:10 +0530

    new change

--- RestAssured/src/test/java/JacksonAPIObject.java
+++ RestAssured/src/test/java/JacksonAPIObject.java
@@ -1,6 +1,8 @@
+import org.assertj.core.util.Arrays;
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -8,6 +10,10 @@ import io.restassured.response.Response;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import static io.restassured.RestAssured.*;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
 public class JacksonAPIObject {
 	String baseuri1 = "https://reqres.in/";
 	String basepath1 = "api/users";
@@ -23,29 +29,69 @@ public class JacksonAPIObject {
 		Userdetails.put("firstName", "rohit");
 		Userdetails.put("lastName", "pachauri");
 		Userdetails.put("age", "23");
-		
+
 		ObjectNode Skills = objmap.createObjectNode();
 		Skills.put("Programming Language", "Python,Java");
 		Skills.put("Automation skills", "selenium");
-		
+
 		Userdetails.set("skills", Skills);
 		try {
-		String Userdetailsinstring=objmap.writerWithDefaultPrettyPrinter().writeValueAsString(Userdetails);
-		System.out.println("User details: "+Userdetailsinstring);
-		
-		String firstname=Userdetails.get("firstName").asText();
-		System.out.println("firstname is: "+firstname);
-		String programmingskill=Userdetails.get("skills").get("Programming Language").asText();
-		System.out.println("programming skill is: "+programmingskill);
-		
-		}
-		catch(JsonProcessingException e) {
+			String Userdetailsinstring = objmap.writerWithDefaultPrettyPrinter().writeValueAsString(Userdetails);
+			System.out.println("User details: " + Userdetailsinstring);
+
+			String firstname = Userdetails.get("firstName").asText();
+			System.out.println("firstname is: " + firstname);
+			String programmingskill = Userdetails.get("skills").get("Programming Language").asText();
+			System.out.println("programming skill is: " + programmingskill);
+
+			Iterator<String> keys = Userdetails.fieldNames();
+			System.out.println("All the fields:------------------------------------");
+			while (keys.hasNext()) {
+				System.out.println(keys.next());
+			}
+			
+			Iterator<JsonNode> values = Userdetails.elements();
+			System.out.println("All the values:------------------------------------");
+			while(values.hasNext()) {
+				System.out.println(values.next());
+			}
+			
+			System.out.println("All the fields and values:------------------------------------");
+			Iterator<Entry<String, JsonNode>> keyvaluepair=Userdetails.fields();
+			while(keyvaluepair.hasNext()) {
+				Entry<String,JsonNode>node=keyvaluepair.next();
+				System.out.println(node.getKey()+" : "+node.getValue());
+			}
+			
+			//Removing node "age" from this objectmapper userdetails----
+			
+			Userdetails.remove("age");
+			System.out.println(" ");
+			System.out.println("All the fields and values after removing age:------------------------------------");
+			Iterator<Entry<String, JsonNode>> keyvaluepairremove=Userdetails.fields();
+			while(keyvaluepairremove.hasNext()) {
+				Entry<String,JsonNode>node=keyvaluepairremove.next();
+				System.out.println(node.getKey()+" : "+node.getValue());
+			}
+			
+			
+			//updating node"first name" from "rohit" to "rohitagain"-------------
+			Userdetails.put("firstName", "rohitagain");
+			System.out.println(" ");
+			System.out.println("All the fields and values after updating firstname:------------------------------------");
+			Iterator<Entry<String, JsonNode>> keyvaluepairupdate=Userdetails.fields();
+			while(keyvaluepairupdate.hasNext()) {
+				Entry<String,JsonNode>node=keyvaluepairupdate.next();
+				System.out.println(node.getKey()+" : "+node.getValue());
+			}
+			
+			
+			
+			
+		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 	}
 
 }
