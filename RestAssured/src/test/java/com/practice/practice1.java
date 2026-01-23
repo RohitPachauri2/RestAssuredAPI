44a82e36ac4db82e58acdb87ec91440995b36a86
rohit <rohitpachauri2@gmail.com>
2026-01-23 08:06:54 +0530

    23Jan2026

--- RestAssured/src/test/java/com/practice/practice1.java
+++ RestAssured/src/test/java/com/practice/practice1.java
@@ -34,7 +34,7 @@ public class practice1 {
 
 	String postendpoint = "/searchProduct";
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void postrequest() {
 		Response res = given().baseUri(baseuri)
 
@@ -45,7 +45,7 @@ public class practice1 {
 
 	String loginpoint = "/verifyLogin";
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void postrequestlogin() {
 		Response res = given().baseUri("https://automationexercise.com/api/").contentType(ContentType.URLENC).formParam("email", "invalid@test.com")
 				.formParam("password", "wrongpassword").when().post("verifyLogin");
@@ -60,5 +60,59 @@ Assert.assertEquals(js.getString("responseCode"),200);
 		Assert.assertEquals(js.getString("message"), "User not found!");
 
 	}
+	@Test(enabled=false)
+	public void postrequestcreateuser() {
+		Response res=given().baseUri(baseuri)
+				// Request Parameters
+                .formParam("name", "Rohit")
+                .formParam("email", "rohit.pachauri2@gmail.com")
+                .formParam("password", "Selenium@1234")
+                .formParam("title", "Mr")
+                .formParam("birth_date", "10")
+                .formParam("birth_month", "05")
+                .formParam("birth_year", "1995")
+                .formParam("firstname", "Rohit")
+                .formParam("lastname", "Pachauri")
+                .formParam("company", "ABC Corp")
+                .formParam("address1", "Street 1")
+                .formParam("address2", "Near Park")
+                .formParam("country", "India")
+                .formParam("zipcode", "110001")
+                .formParam("state", "Delhi")
+                .formParam("city", "New Delhi")
+                .formParam("mobile_number", "9876543210")
+				.when().post("/createAccount");
+		res.prettyPrint();
+		System.out.println("Status Line: "+res.statusLine());
+		
+	}
+	
+	@Test(enabled=false)
+	public void testdelete() {
+		Response res=given()
+				.baseUri(baseuri)
+				.formParam("email", "rohit.pachauri2@gmail.com")
+                .formParam("password", "Selenium@1234")
+                .when()
+                .delete("/deleteAccount");
+		ValidatableResponse valres=res.then().statusCode(200);
+		res.prettyPrint();
+	}
+	@Test
+	public void testrequestemail() {
+		Response res=given()
+				.baseUri(baseuri)
+				.formParam("email", "rohitpachauri2@gmail.com")
+                .when()
+                .get("/getUserDetailByEmail");
+		ValidatableResponse valres=res.then().statusCode(200);
+		res.prettyPrint();
+		String strres=res.asString();
+		String jsonOnly = strres.replaceAll("<[^>]*>", "");
+		JsonPath js=new JsonPath(jsonOnly);
+		System.out.println(js.getString("user.name"));
+	}
+	
+	
 
 }
